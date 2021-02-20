617b7292c05a9018b17979bdd8c954e68cfb7c9a
mattyBrappicus <56854957+mattyBrappicus@users.noreply.github.com>
2021-02-19 20:46:11 -0700

    new edits. removed whitespace. buttons functioning.

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-function sound1() {
-  var audio1 = new Audio('frog.wav');
+function misc_yeaah() {
+  var audio1 = new Audio('../GitHub/audio/misc_yeaah.wav');
   audio1.play();
 }
-function sound2() {
-  var audio2 = new Audio('close.wav');
+function g_no_2() {
+  var audio2 = new Audio('../GitHub/audio/g_no_2.wav');
   audio2.play();
 }
-function sound3() {
-  var audio3 = new Audio('hand.wav');
+function misc_sex() {
+  var audio3 = new Audio('../GitHub/audio/misc_sex.wav');
   audio3.play();
 }
-function sound4() {
-  var audio4 = new Audio('tweet.wav');
+function c_sandwich_2() {
+  var audio4 = new Audio('../GitHub/audio/c_sandwich_2.wav');
   audio4.play();
 }
-function sound5() {
+function w_hug() {
+  var audio5 = new Audio('../GitHub/audio/w_hug.wav');
+  audio5.play();
 }
-function sound6() {
+function misc_over9000() {
+  var audio6 = new Audio('../GitHub/audio/misc_over9000.wav');
+  audio6.play();
+}
+function name() {
+  var audiox = new Audio('../GitHub/audio/name.wav');
+  audiox.play();
 }
