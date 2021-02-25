f5841442db47568bd64888b38da51a2eefb7ddf7
mattyBrappicus <56854957+mattyBrappicus@users.noreply.github.com>
2021-02-24 21:03:38 -0700

    js working properly

--- index.js
+++ index.js
@@ -3,354 +3,354 @@ function misc_yeaah() {
   audio1.play();
 }
 function g_no_2() {
-  var audio2 = new Audio('../GitHub/audio/g_no_2.wav');
+  var audio2 = new Audio('g_no_2.wav');
   audio2.play();
 }
 function misc_sex() {
-  var audio3 = new Audio('../GitHub/audio/misc_sex.wav');
+  var audio3 = new Audio('misc_sex.wav');
   audio3.play();
 }
 function c_sandwich_2() {
-  var audio4 = new Audio('../GitHub/audio/c_sandwich_2.wav');
+  var audio4 = new Audio('c_sandwich_2.wav');
   audio4.play();
 }
 function w_hug() {
-  var audio5 = new Audio('../GitHub/audio/w_hug.wav');
+  var audio5 = new Audio('w_hug.wav');
   audio5.play();
 }
 function p_snisner() {
-  var audio6 = new Audio('../GitHub/audio/p_snisner.wav');
+  var audio6 = new Audio('p_snisner.wav');
   audio6.play();
 }
 function c_aa() {
-  var audio7 = new Audio('../GitHub/audio/c_aa.wav');
+  var audio7 = new Audio('c_aa.wav');
   audio7.play();
 }
 function c_anunenuna() {
-  var audio8 = new Audio('../GitHub/audio/c_anunenuna.wav');
+  var audio8 = new Audio('c_anunenuna.wav');
   audio8.play();
 }
 function c_cruelhouse() {
-  var audio9 = new Audio('../GitHub/audio/c_cruelhouse.wav');
+  var audio9 = new Audio('c_cruelhouse.wav');
   audio9.play();
 }
 function c_hellOH() {
-  var audio10 = new Audio('../GitHub/audio/c_hellOH.wav');
+  var audio10 = new Audio('c_hellOH.wav');
   audio10.play();
 }
 function c_hilary() {
-  var audio11 = new Audio('../GitHub/audio/c_hilary.wav');
+  var audio11 = new Audio('c_hilary.wav');
   audio11.play();
 }
 function c_ohmygod() {
-  var audio12 = new Audio('../GitHub/audio/c_ohmygod.wav');
+  var audio12 = new Audio('c_ohmygod.wav');
   audio12.play();
 }
 function c_phonesdead() {
-  var audio13 = new Audio('../GitHub/audio/c_phonesdead.wav');
+  var audio13 = new Audio('c_phonesdead.wav');
   audio13.play();
 }
 function c_prez() {
-  var audio14 = new Audio('../GitHub/audio/c_prez.wav');
+  var audio14 = new Audio('c_prez.wav');
   audio14.play();
 }
 function c_rewr() {
-  var audio15 = new Audio('../GitHub/audio/c_rewr.wav');
+  var audio15 = new Audio('c_rewr.wav');
   audio15.play();
 }
 function c_sandwich_1() {
-  var audio16 = new Audio('../GitHub/audio/c_sandwich_1.wav');
+  var audio16 = new Audio('c_sandwich_1.wav');
   audio16.play();
 }
 function c_sandwich_3() {
-  var audio17 = new Audio('../GitHub/audio/c_sandwich_3.wav');
+  var audio17 = new Audio('c_sandwich_3.wav');
   audio17.play();
 }
 function c_scream() {
-  var audio18 = new Audio('../GitHub/audio/c_scream.wav');
+  var audio18 = new Audio('c_scream.wav');
   audio18.play();
 }
 function c_shame_1() {
-  var audio19 = new Audio('../GitHub/audio/c_shame_1.wav');
+  var audio19 = new Audio('c_shame_1.wav');
   audio19.play();
 }
 function c_shame_2() {
-  var audio20 = new Audio('../GitHub/audio/c_shame_2.wav');
+  var audio20 = new Audio('c_shame_2.wav');
   audio20.play();
 }
 function c_youredead() {
-  var audio21 = new Audio('../GitHub/audio/c_youredead.wav');
+  var audio21 = new Audio('c_youredead.wav');
   audio21.play();
 }
 function w_andmymom() {
-  var audio22 = new Audio('../GitHub/audio/w_andmymom.wav');
+  var audio22 = new Audio('w_andmymom.wav');
   audio22.play();
 }
 function w_checkitout() {
-  var audio23 = new Audio('../GitHub/audio/w_checkitout.wav');
+  var audio23 = new Audio('w_checkitout.wav');
   audio23.play();
 }
 function w_feellikethis() {
-  var audio24 = new Audio('../GitHub/audio/w_feellikethis.wav');
+  var audio24 = new Audio('w_feellikethis.wav');
   audio24.play();
 }
 function w_heaven() {
-  var audio25 = new Audio('../GitHub/audio/w_heaven.wav');
+  var audio25 = new Audio('w_heaven.wav');
   audio25.play();
 }
 function w_hug() {
-  var audio26 = new Audio('../GitHub/audio/w_hug.wav');
+  var audio26 = new Audio('w_hug.wav');
   audio26.play();
 }
 function w_kickbutt() {
-  var audio27 = new Audio('../GitHub/audio/w_kickbutt.wav');
+  var audio27 = new Audio('w_kickbutt.wav');
   audio27.play();
 }
 function w_killer() {
-  var audio28 = new Audio('../GitHub/audio/w_killer.wav');
+  var audio28 = new Audio('w_killer.wav');
   audio28.play();
 }
 function w_mom() {
-  var audio29 = new Audio('../GitHub/audio/w_mom.wav');
+  var audio29 = new Audio('w_mom.wav');
   audio29.play();
 }
 function w_numbers() {
-  var audio30 = new Audio('../GitHub/audio/w_numbers.wav');
+  var audio30 = new Audio('w_numbers.wav');
   audio30.play();
 }
 function w_onlyhuman() {
-  var audio31 = new Audio('../GitHub/audio/w_onlyhuman.wav');
+  var audio31 = new Audio('w_onlyhuman.wav');
   audio31.play();
 }
 function w_penis() {
-  var audio32 = new Audio('../GitHub/audio/w_penis.wav');
+  var audio32 = new Audio('w_penis.wav');
   audio32.play();
 }
 function w_pizzahut() {
-  var audio33 = new Audio('../GitHub/audio/w_pizzahut.wav');
+  var audio33 = new Audio('w_pizzahut.wav');
   audio33.play();
 }
 function w_scream() {
-  var audio34 = new Audio('../GitHub/audio/w_scream.wav');
+  var audio34 = new Audio('w_scream.wav');
   audio34.play();
 }
 function w_this() {
-  var audio35 = new Audio('../GitHub/audio/w_this.wav');
+  var audio35 = new Audio('w_this.wav');
   audio35.play();
 }
 function w_uhuhgirls() {
-  var audio36 = new Audio('../GitHub/audio/w_uhuhgirls.wav');
+  var audio36 = new Audio('w_uhuhgirls.wav');
   audio36.play();
 }
 function w_weloveyou() {
-  var audio37 = new Audio('../GitHub/audio/w_weloveyou.wav');
+  var audio37 = new Audio('w_weloveyou.wav');
   audio37.play();
 }
 function g_bell() {
-  var audio38 = new Audio('../GitHub/audio/g_bell.wav');
+  var audio38 = new Audio('g_bell.wav');
   audio38.play();
 }
 function g_cookies() {
-  var audio39 = new Audio('../GitHub/audio/g_cookies.wav');
+  var audio39 = new Audio('g_cookies.wav');
   audio39.play();
 }
 function g_HAH() {
-  var audio40 = new Audio('../GitHub/audio/g_HAH.wav');
+  var audio40 = new Audio('g_HAH.wav');
   audio40.play();
 }
 function g_hahuhe() {
-  var audio41 = new Audio('../GitHub/audio/g_hahuhe.wav');
+  var audio41 = new Audio('g_hahuhe.wav');
   audio41.play();
 }
 function g_no_1() {
-  var audio42 = new Audio('../GitHub/audio/g_no_1.wav');
+  var audio42 = new Audio('g_no_1.wav');
   audio42.play();
 }
 function g_no_3() {
-  var audio43 = new Audio('../GitHub/audio/g_no_3.wav');
+  var audio43 = new Audio('g_no_3.wav');
   audio43.play();
 }
 function g_rubbingmycock() {
-  var audio44 = new Audio('../GitHub/audio/g_rubbingmycock.wav');
+  var audio44 = new Audio('g_rubbingmycock.wav');
   audio44.play();
 }
 function g_song() {
-  var audio45 = new Audio('../GitHub/audio/g_song.wav');
+  var audio45 = new Audio('g_song.wav');
   audio45.play();
 }
 function g_splelps() {
-  var audio46 = new Audio('../GitHub/audio/g_splelps.wav');
+  var audio46 = new Audio('g_splelps.wav');
   audio46.play();
 }
 function g_swindle() {
-  var audio47 = new Audio('../GitHub/audio/g_swindle.wav');
+  var audio47 = new Audio('g_swindle.wav');
   audio47.play();
 }
 function g_whatsawoman() {
-  var audio48 = new Audio('../GitHub/audio/g_whatsawoman.wav');
+  var audio48 = new Audio('g_whatsawoman.wav');
   audio48.play();
 }
 function g_yeeeaaa() {
-  var audio49 = new Audio('../GitHub/audio/g_yeeeaaa.wav');
+  var audio49 = new Audio('g_yeeeaaa.wav');
   audio49.play();
 }
 function p_aeaeae() {
-  var audio50 = new Audio('../GitHub/audio/p_aeaeae.wav');
+  var audio50 = new Audio('p_aeaeae.wav');
   audio50.play();
 }
 function p_directions() {
-  var audio51 = new Audio('../GitHub/audio/p_directions.wav');
+  var audio51 = new Audio('p_directions.wav');
   audio51.play();
 }
 function p_humuliating() {
-  var audio5x = new Audio('../GitHub/audio/p_humuliating.wav');
+  var audio5x = new Audio('p_humuliating.wav');
   audio5x.play();
 }
 function p_mem() {
-  var audio52 = new Audio('../GitHub/audio/p_mem.wav');
+  var audio52 = new Audio('p_mem.wav');
   audio52.play();
 }
 function p_nope() {
-  var audio53 = new Audio('../GitHub/audio/p_nope.wav');
+  var audio53 = new Audio('p_nope.wav');
   audio53.play();
 }
 function p_phone() {
-  var audio54 = new Audio('../GitHub/audio/p_phone.wav');
+  var audio54 = new Audio('p_phone.wav');
   audio54.play();
 }
 function p_phone_1() {
-  var audio55 = new Audio('../GitHub/audio/p_phone_1.wav');
+  var audio55 = new Audio('p_phone_1.wav');
   audio55.play();
 }
 function p_phone_2() {
-  var audio56 = new Audio('../GitHub/audio/p_phone_2.wav');
+  var audio56 = new Audio('p_phone_2.wav');
   audio56.play();
 }
 function p_phone_3() {
-  var audio57 = new Audio('../GitHub/audio/p_phone_3.wav');
+  var audio57 = new Audio('p_phone_3.wav');
   audio57.play();
 }
 function p_phone_4() {
-  var audio58 = new Audio('../GitHub/audio/p_phone_4.wav');
+  var audio58 = new Audio('p_phone_4.wav');
   audio58.play();
 }
 function p_phone_5() {
-  var audio59 = new Audio('../GitHub/audio/p_phone_5.wav');
+  var audio59 = new Audio('p_phone_5.wav');
   audio59.play();
 }
 function p_sandwich() {
-  var audio60 = new Audio('../GitHub/audio/p_sandwich.wav');
+  var audio60 = new Audio('p_sandwich.wav');
   audio60.play();
 }
 function p_statement() {
-  var audio61 = new Audio('../GitHub/audio/p_statement.wav');
+  var audio61 = new Audio('p_statement.wav');
   audio61.play();
 }
 function p_warmmemory() {
-  var audio62 = new Audio('../GitHub/audio/p_warmmemory.wav');
+  var audio62 = new Audio('p_warmmemory.wav');
   audio62.play();
 }
 function p_WEEW() {
-  var audio63 = new Audio('../GitHub/audio/p_WEEW.wav');
+  var audio63 = new Audio('p_WEEW.wav');
   audio63.play();
 }
 function p_women() {
-  var audio64 = new Audio('../GitHub/audio/p_women.wav');
+  var audio64 = new Audio('p_women.wav');
   audio64.play();
 }
 function h_erection() {
-  var audio6x = new Audio('../GitHub/audio/h_erection.wav');
+  var audio6x = new Audio('h_erection.wav');
   audio6x.play();
 }
 function h_idc() {
-  var audio6x = new Audio('../GitHub/audio/h_idc.wav');
+  var audio6x = new Audio('h_idc.wav');
   audio6x.play();
 }
 function h_illsay() {
-  var audio6x = new Audio('../GitHub/audio/h_illsay.wav');
+  var audio6x = new Audio('h_illsay.wav');
   audio6x.play();
 }
 function h_manivilization() {
-  var audio6x = new Audio('../GitHub/audio/h_manivilization.wav');
+  var audio6x = new Audio('h_manivilization.wav');
   audio6x.play();
 }
 function v_chinese() {
-  var audio6x = new Audio('../GitHub/audio/v_chinese.wav');
+  var audio6x = new Audio('v_chinese.wav');
   audio6x.play();
 }
 function v_idc() {
-  var audio7x = new Audio('../GitHub/audio/v_idc.wav');
+  var audio7x = new Audio('v_idc.wav');
   audio7x.play();
 }
 function v_lulz() {
-  var audio7x = new Audio('../GitHub/audio/v_lulz.wav');
+  var audio7x = new Audio('v_lulz.wav');
   audio7x.play();
 }
 function v_objective() {
-  var audio7x = new Audio('../GitHub/audio/v_objective.wav');
+  var audio7x = new Audio('v_objective.wav');
   audio7x.play();
 }
 function r_banter() {
-  var audio73 = new Audio('../GitHub/audio/r_banter.wav');
+  var audio73 = new Audio('r_banter.wav');
   audio73.play();
 }
 function r_death() {
-  var audio7x = new Audio('../GitHub/audio/r_death.wav');
+  var audio7x = new Audio('r_death.wav');
   audio7x.play();
 }
 function r_douche() {
-  var audio7x = new Audio('../GitHub/audio/r_douche.wav');
+  var audio7x = new Audio('r_douche.wav');
   audio7x.play();
 }
 function r_sas() {
-  var audio7x = new Audio('../GitHub/audio/r_sas.wav');
+  var audio7x = new Audio('r_sas.wav');
   audio7x.play();
 }
 function a_aww() {
-  var audio7x = new Audio('../GitHub/audio/a_aww.wav');
+  var audio7x = new Audio('a_aww.wav');
   audio7x.play();
 }
 function a_fatty() {
-  var audio7x = new Audio('../GitHub/audio/a_fatty.wav');
+  var audio7x = new Audio('a_fatty.wav');
   audio7x.play();
 }
 function misc_brain1() {
-  var audio7x = new Audio('../GitHub/audio/misc_brain1.wav');
+  var audio7x = new Audio('misc_brain1.wav');
   audio7x.play();
 }
 function misc_brain2() {
-  var audio8x = new Audio('../GitHub/audio/misc_brain2.wav');
+  var audio8x = new Audio('misc_brain2.wav');
   audio8x.play();
 }
 function misc_brain3() {
-  var audio8x = new Audio('../GitHub/audio/misc_brain3.wav');
+  var audio8x = new Audio('misc_brain3.wav');
   audio8x.play();
 }
 function misc_die() {
-  var audio8x = new Audio('../GitHub/audio/misc_die.wav');
+  var audio8x = new Audio('misc_die.wav');
   audio8x.play();
 }
 function misc_henryfirth() {
-  var audio8x = new Audio('../GitHub/audio/misc_henryfirth.wav');
+  var audio8x = new Audio('misc_henryfirth.wav');
   audio8x.play();
 }
 function misc_memorial() {
-  var audio8x = new Audio('../GitHub/audio/misc_memorial.wav');
+  var audio8x = new Audio('misc_memorial.wav');
   audio8x.play();
 }
 function misc_ohok() {
-  var audio8x = new Audio('../GitHub/audio/misc_ohok.wav');
+  var audio8x = new Audio('misc_ohok.wav');
   audio8x.play();
 }
 function misc_over9000() {
-  var audio8x = new Audio('../GitHub/audio/misc_over9000.wav');
+  var audio8x = new Audio('misc_over9000.wav');
   audio8x.play();
 }
 function misc_reer() {
-  var audio8x = new Audio('../GitHub/audio/misc_reer.wav');
+  var audio8x = new Audio('misc_reer.wav');
   audio8x.play();
 }
 function name() {
-  var audio8x = new Audio('../GitHub/audio/name.wav');
+  var audio8x = new Audio('name.wav');
   audio8x.play();
 }
